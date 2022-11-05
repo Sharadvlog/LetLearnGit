eda165dd4321679bf2a7eb45af0ec2baf28ede5f
sharad shivaji sable <sssable1999@gmail.com>
2022-11-05 22:29:09 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=4,b=6;
+        System.out.println(a*b);
+    }
+}
--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String[] args) {
         int a=10,b=12;
-        System.out.println(a+b);
+        System.out.println("the sum is" + a+b);
     }
 }
